b44c1541e9942647651b1ea0f4c5856fa364fc3a
vilasini-v <gulkavarsh@gmail.com>
2024-06-03 17:52:01 +0530

    worked on work.js

--- src/components/work.js
+++ src/components/work.js
@@ -25,13 +25,49 @@ const work = () => {
         {
             name:"UI/UX zero to hero",
             year:"2023",
-            desc:"A website that bridges the gap between nurseries and buyers, promoting sustainable practices in line with SDGs 15 (Life on Land) and 2 (Zero Hunger).",
-            techStacks:"HTML5, CSS, JS, Node JS, Python"
+            desc:"Created a FIGMA mockup for Music App and was ranked in top 3 out of 80 participants.",
+            techStacks:""
         },
+        {
+            name:"IEEE Robotics Society Hackathon",
+            year:"2023",
+            desc:"Implemented a machine learning model to determine a patient’s condition with an accuracy of 0.76. Top 5 out of 60 teams.",
+            techStacks:"Python"
+        },
+        {
+            name:"SMART INDIA HACKATHON",
+            year:"2023",
+            desc:"Devised a web-app that helps students network with professors for research partnerships and mentorship programmes.",
+            techStacks:"React JS, Django, Sci-kit learn"
+        }
+    ];
+    const projects=[
+        {
+        name:'Affirmations generator',
+        link:'https://affirmations-iota.vercel.app/',
+        year:'2023',
+        desc:'A frontend project that generates affirmations with the option to share it to twitter/X.',
+        techStacks:"HTML, CSS, JavaScript"
+    },
+    {
+        name:'Arcade',
+        link:'https://arcade-navy.vercel.app/',
+        year:'2024',
+        desc:'An arcade with mini games made with javascript such as rock-paper-scissors, coin-flip, and guess-the-number',
+        techStacks:"Javascript, HTML, CSS, React JS"
+    }
+    ];
+    const [visibleCount, setVisibleCount] = useState(1);
+    const handleViewMore = () => {
+        setVisibleCount((prevCount) => prevCount + 2);
+      };
+      const handleViewLess = () => {
+        setVisibleCount((prevCount) => (prevCount - 2 > 0 ? prevCount - 2 : 2));
+      };
 
-    ]
   return (
-    <div>
+    <div className='work-container'>
+        
       
     </div>
   )
